f3dfebf2ce70672861f2a1442ef2228e8cf37d00
str4d <str4d@mail.i2p>
2014-04-20 06:50:38 +0000

    Renamed variable to prevent confusion

--- src/FieldElement.java
+++ src/FieldElement.java
@@ -6,10 +6,10 @@ public class FieldElement {
 	public static final FieldElement ONE = new FieldElement(BigInteger.ONE);
 	public static final FieldElement TWO = new FieldElement(BigInteger.valueOf(2));
 
-	final BigInteger x;
+	final BigInteger bi;
 
-	public FieldElement(BigInteger x) {
-		this.x = x;
+	public FieldElement(BigInteger bi) {
+		this.bi = bi;
 	}
 
 	/**
@@ -24,11 +24,11 @@ public class FieldElement {
 		for (int i = 0; i < val.length; i++) {
 			out[i] = val[val.length-1-i];
 		}
-		this.x = new BigInteger(out).and(Constants.un);
+		this.bi = new BigInteger(out).and(Constants.un);
 	}
 
 	public byte[] toByteArray() {
-		byte[] in = x.toByteArray();
+		byte[] in = bi.toByteArray();
 		byte[] out = new byte[in.length];
 		for (int i = 0; i < in.length; i++) {
 			out[i] = in[in.length-1-i];
@@ -37,31 +37,31 @@ public class FieldElement {
 	}
 
 	public boolean isNonZero() {
-		return !x.equals(BigInteger.ZERO);
+		return !bi.equals(BigInteger.ZERO);
 	}
 
 	public boolean isNegative() {
-		return x.testBit(0);
+		return bi.testBit(0);
 	}
 
 	public FieldElement add(FieldElement val) {
-		return new FieldElement(x.add(val.x));
+		return new FieldElement(bi.add(val.bi));
 	}
 
 	public FieldElement subtract(FieldElement val) {
-		return new FieldElement(x.subtract(val.x));
+		return new FieldElement(bi.subtract(val.bi));
 	}
 
 	public FieldElement negate() {
-		return new FieldElement(Constants.q.subtract(x));
+		return new FieldElement(Constants.q.subtract(bi));
 	}
 
 	public FieldElement multiply(FieldElement val) {
-		return new FieldElement(x.multiply(val.x).mod(Constants.q));
+		return new FieldElement(bi.multiply(val.bi).mod(Constants.q));
 	}
 
 	public FieldElement square() {
-		return new FieldElement(x.modPow(BigInteger.valueOf(2), Constants.q));
+		return new FieldElement(bi.modPow(BigInteger.valueOf(2), Constants.q));
 	}
 
 	public FieldElement squareAndDouble() {
@@ -73,6 +73,6 @@ public class FieldElement {
 	}
 
 	public FieldElement modPow(BigInteger e, BigInteger m) {
-		return new FieldElement(x.modPow(e, m));
+		return new FieldElement(bi.modPow(e, m));
 	}
 }
