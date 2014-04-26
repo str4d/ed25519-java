53d514a8789598178aaffd0bba76d72866db5961
str4d <str4d@mail.i2p>
2014-04-26 01:31:42 +0000

    Started interface for algorithm-specific FieldElement encoding

--- src/net/i2p/crypto/eddsa/EdDSAEncoding.java
+++ src/net/i2p/crypto/eddsa/EdDSAEncoding.java
@@ -1,5 +1,15 @@
 package net.i2p.crypto.eddsa;
 
-public class EdDSAEncoding {
+import net.i2p.crypto.eddsa.math.Curve;
+import net.i2p.crypto.eddsa.math.FieldElement;
 
+/**
+ * Common interface for all (b-1)-bit encodings of elements
+ * of EdDSA finite fields.
+ * @author str4d
+ *
+ */
+public interface EdDSAEncoding {
+    public byte[] toEncoding(FieldElement x);
+    public FieldElement toElement(Curve curve, byte[] in);
 }
