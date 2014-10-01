99889ae277bf4e59c403e131c8b72b9b6f913351
BloodyRookie <nemproject@gmx.de>
2014-10-01 13:23:01 +0200

    Tests for Ed25519LittleEndianEncoding.java.

---
+++ test/net/i2p/crypto/eddsa/math/MathUtils.java
@@ -0,0 +1,78 @@
+package net.i2p.crypto.eddsa.math;
+
+import net.i2p.crypto.eddsa.Utils;
+import net.i2p.crypto.eddsa.math.ed25519.*;
+
+import java.math.BigInteger;
+/**
+ * Utility class to help with calculation.
+ */
+public class MathUtils {
+	private static final int[] exponents = {0, 26, 26 + 25, 2*26 + 25, 2*26 + 2*25, 3*26 + 2*25, 3*26 + 3*25, 4*26 + 3*25, 4*26 + 4*25, 5*26 + 4*25};
+
+	/**
+	 * Gets the underlying finite field with q=2^255 - 19 elements.
+	 *
+	 * @return The finite field.
+	 */
+	public static Field getField() {
+		return new Field(
+				256, // b
+				Utils.hexToBytes("edffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7f"), // q
+				new Ed25519LittleEndianEncoding());
+	}
+
+	/**
+	 * Converts a 2^25.5 bit representation to a BigInteger.
+	 * Value: 2^exponents[0] * t[0] + 2^exponents[1] * t[1] + ... + 2^exponents[9] * t[9]
+	 *
+	 * @param t The 2^25.5 bit representation.
+	 * @return The BigInteger.
+	 */
+	public static BigInteger toBigInteger(final int[] t) {
+		BigInteger b = BigInteger.ZERO;
+		for (int i=0; i<10; i++) {
+			b = b.add(BigInteger.ONE.multiply(BigInteger.valueOf(t[i])).shiftLeft(exponents[i]));
+		}
+
+		return b;
+	}
+
+	/**
+	 * Converts a 32 byte representation to a BigInteger.
+	 * Value: bytes[0] + 2^8 * bytes[1] + ... + 2^248 * bytes[31]
+	 *
+	 * @param bytes The 32 byte representation.
+	 * @return The BigInteger.
+	 */
+	public static BigInteger toBigInteger(final byte[] bytes) {
+		BigInteger b = BigInteger.ZERO;
+		for (int i=0; i<32; i++) {
+			b = b.add(BigInteger.ONE.multiply(BigInteger.valueOf(bytes[i] & 0xff)).shiftLeft(i * 8));
+		}
+
+		return b;
+	}
+
+	/**
+	 * Converts a BigInteger to a little endian 32 byte representation.
+	 *
+	 * @param b The BigInteger.
+	 * @return The 32 byte representation.
+	 */
+	public static byte[] toByteArray(final BigInteger b) {
+		if (b.compareTo(BigInteger.ONE.shiftLeft(256)) >= 0) {
+			throw new RuntimeException("only numbers < 2^256 are allowed");
+		}
+		final byte[] bytes = new byte[32];
+		final byte[] original = b.toByteArray();
+
+		// Although b < 2^256, toByteArray can have length > 32 with some bytes set to 0.
+		final int offset = original.length > 32? original.length - 32 : 0;
+		for (int i=0; i<original.length - offset; i++) {
+			bytes[original.length - i - offset - 1] = original[i + offset];
+		}
+
+		return bytes;
+	}
+}
--- test/net/i2p/crypto/eddsa/math/ed25519/Ed25519LittleEndianEncodingTest.java
+++ test/net/i2p/crypto/eddsa/math/ed25519/Ed25519LittleEndianEncodingTest.java
@@ -1,21 +1,91 @@
 package net.i2p.crypto.eddsa.math.ed25519;
 
+import net.i2p.crypto.eddsa.math.*;
 import org.hamcrest.core.IsEqual;
 import org.junit.*;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+/**
+ * Tests rely on the BigInteger class.
+ */
 public class Ed25519LittleEndianEncodingTest {
 
+	// 2^255 - 19
+	private static final BigInteger q = new BigInteger("7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffed", 16);
+	private static final SecureRandom random = new SecureRandom();
+
 	@Test
-	public void decodeReturnsCorrectResult() {
-		// Arange:
-		final Ed25519LittleEndianEncoding encoding = new Ed25519LittleEndianEncoding();
-		final byte[] byteRep = new byte[32];
-		byteRep[31] = (byte)(1 << 6);
+	public void encodeReturnsCorrectByteArrayForSimpleFieldElements() {
+		// Arrange:
+		final int[] t1 = new int[10];
+		final int[] t2 = new int[10];
+		t2[0] = 1;
+		final FieldElement fieldElement1 = new Ed25519FieldElement(MathUtils.getField(), t1);
+		final FieldElement fieldElement2 = new Ed25519FieldElement(MathUtils.getField(), t2);
 
 		// Act:
-		final Ed25519FieldElement element = (Ed25519FieldElement)encoding.decode(byteRep);
+		final byte[] bytes1 = MathUtils.getField().getEncoding().encode(fieldElement1);
+		final byte[] bytes2 = MathUtils.getField().getEncoding().encode(fieldElement2);
 
 		// Assert:
-		Assert.assertThat(element.t[9], IsEqual.equalTo(1 << 24));
+		Assert.assertThat(bytes1, IsEqual.equalTo(MathUtils.toByteArray(BigInteger.ZERO)));
+		Assert.assertThat(bytes2, IsEqual.equalTo(MathUtils.toByteArray(BigInteger.ONE)));
+	}
+
+	@Test
+	public void encodeReturnsCorrectByteArray() {
+		for (int i=0; i<10000; i++){
+			// Arrange:
+			final int[] t = new int[10];
+			for (int j=0; j<10; j++) {
+				t[j] = random.nextInt(1 << 28) - (1 << 27);
+			}
+			final FieldElement fieldElement1 = new Ed25519FieldElement(MathUtils.getField(), t);
+			final BigInteger b = MathUtils.toBigInteger(t);
+
+			// Act:
+			final byte[] bytes = MathUtils.getField().getEncoding().encode(fieldElement1);
+
+			// Assert:
+			Assert.assertThat(bytes, IsEqual.equalTo(MathUtils.toByteArray(b.mod(q))));
+		}
+	}
+
+	@Test
+	public void decodeReturnsCorrectFieldElementForSimpleByteArrays() {
+		// Arrange:
+		final byte[] bytes1 = new byte[32];
+		final byte[] bytes2 = new byte[32];
+		bytes2[0] = 1;
+
+		// Act:
+		final Ed25519FieldElement f1 = (Ed25519FieldElement)MathUtils.getField().getEncoding().decode(bytes1);
+		final Ed25519FieldElement f2 = (Ed25519FieldElement)MathUtils.getField().getEncoding().decode(bytes2);
+		final BigInteger b1 = MathUtils.toBigInteger(f1.t);
+		final BigInteger b2 = MathUtils.toBigInteger(f2.t);
+
+		// Assert:
+		Assert.assertThat(b1, IsEqual.equalTo(BigInteger.ZERO));
+		Assert.assertThat(b2, IsEqual.equalTo(BigInteger.ONE));
+	}
+
+	@Test
+	public void decodeReturnsCorrectFieldElement() {
+		for (int i=0; i<10000; i++) {
+			// Arrange:
+			final byte[] bytes = new byte[32];
+			random.nextBytes(bytes);
+			bytes[31] = (byte)(bytes[31] & 0x7f);
+			final BigInteger b1 = MathUtils.toBigInteger(bytes);
+
+			// Act:
+			final Ed25519FieldElement f = (Ed25519FieldElement)MathUtils.getField().getEncoding().decode(bytes);
+			final BigInteger b2 = MathUtils.toBigInteger(f.t).mod(q);
+
+			// Assert:
+			Assert.assertThat(b2, IsEqual.equalTo(b1));
+		}
 	}
 }
