b918755a47d461bcb869c7d48bdc2184543ad48c
Lyor Goldstein <lyor.goldstein@gmail.com>
2016-10-12 09:39:21 +0300

    Added EdDSASecurityProvider that can be registered in the JCE

--- src/net/i2p/crypto/eddsa/EdDSAKey.java
+++ src/net/i2p/crypto/eddsa/EdDSAKey.java
@@ -16,12 +16,16 @@ import net.i2p.crypto.eddsa.spec.EdDSAParameterSpec;
 /**
  * Common interface for all EdDSA keys.
  * @author str4d
- *
  */
 public interface EdDSAKey {
+    /**
+     * The reported key algorithm for all EdDSA keys
+     */
+    String KEY_ALGORITHM = "EdDSA";
+
     /**
      * return a parameter specification representing the EdDSA domain
      * parameters for the key.
      */
-    public EdDSAParameterSpec getParams();
+    EdDSAParameterSpec getParams();
 }
