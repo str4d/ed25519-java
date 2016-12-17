b4496025dcbfdcb9d2ad20a63c6c1407521f1c9f
str4d <str4d@mail.i2p>
2016-12-17 15:49:59 +1300

    Implement provider mappings between OIDs and algorithm names

--- src/net/i2p/crypto/eddsa/EdDSASecurityProvider.java
+++ src/net/i2p/crypto/eddsa/EdDSASecurityProvider.java
@@ -38,9 +38,22 @@ public class EdDSASecurityProvider extends Provider {
     }
 
     protected void setup() {
-        // see https://docs.oracle.com/javase/8/docs/technotes/guides/security/crypto/HowToImplAProvider.html
-        put("KeyPairGenerator." + EdDSAKey.KEY_ALGORITHM, "net.i2p.crypto.eddsa.KeyPairGenerator");
+        // See https://docs.oracle.com/javase/8/docs/technotes/guides/security/crypto/HowToImplAProvider.html
         put("KeyFactory." + EdDSAKey.KEY_ALGORITHM, "net.i2p.crypto.eddsa.KeyFactory");
+        put("KeyPairGenerator." + EdDSAKey.KEY_ALGORITHM, "net.i2p.crypto.eddsa.KeyPairGenerator");
         put("Signature." + EdDSAEngine.SIGNATURE_ALGORITHM, "net.i2p.crypto.eddsa.EdDSAEngine");
+
+        // OID Mappings
+        // See section "Mapping from OID to name".
+        // The Key* -> OID mappings correspond to the default algorithm in KeyPairGenerator.
+        //
+        // From draft-ieft-curdle-pkix-04:
+        //   id-Ed25519   OBJECT IDENTIFIER ::= { 1 3 101 112 }
+        put("Alg.Alias.KeyFactory.1.3.101.112", EdDSAKey.KEY_ALGORITHM);
+        put("Alg.Alias.KeyFactory.OID.1.3.101.112", EdDSAKey.KEY_ALGORITHM);
+        put("Alg.Alias.KeyPairGenerator.1.3.101.112", EdDSAKey.KEY_ALGORITHM);
+        put("Alg.Alias.KeyPairGenerator.OID.1.3.101.112", EdDSAKey.KEY_ALGORITHM);
+        put("Alg.Alias.Signature.1.3.101.112", EdDSAEngine.SIGNATURE_ALGORITHM);
+        put("Alg.Alias.Signature.OID.1.3.101.112", EdDSAEngine.SIGNATURE_ALGORITHM);
     }
 }
